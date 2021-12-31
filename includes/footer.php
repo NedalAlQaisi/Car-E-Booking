2eed391f96f80fef8eef881a09107c28609c554a
NedalAlQaisi <79810000+NedalAlQaisi@users.noreply.github.com>
2021-12-31 13:54:33 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+    <div class="container">
+
+        <div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s"
+            style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+            Copyright 2021 Graduation Project <a href="#">Aisha & Mahmoud <a />
+        </div>
+    </div>
